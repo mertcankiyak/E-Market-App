3224ec6e03189c417d7a111e0b30744de57d2d97
mertcankiyak <70319395+mertcankiyak@users.noreply.github.com>
2025-07-04 21:52:35 +0300

    Initial Commit

---
+++ E-Market-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Market-App
+//
+//  Created by Mert on 4.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ E-Market-AppTests/E_Market_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  E_Market_AppTests.swift
+//  E-Market-AppTests
+//
+//  Created by Mert on 4.07.2025.
+//
+
+import Testing
+@testable import E_Market_App
+
+struct E_Market_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
