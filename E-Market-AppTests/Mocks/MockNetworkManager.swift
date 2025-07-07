d26f59b97fb53494d3a12ad7356da5c09bf1b5ba
mertcankiyak <70319395+mertcankiyak@users.noreply.github.com>
2025-07-07 08:37:19 +0300

    mock data inital commit

--- E-Market-AppTests/Mocks/MockNetworkManager.swift
+++ E-Market-AppTests/Mocks/MockNetworkManager.swift
@@ -10,43 +10,32 @@ import UIKit
 @testable import E_Market_App 
 
 
-import Foundation
-@testable import E_Market_App // Projenizin adı
-
 class MockNetworkManager: NetworkManagingProtocol {
    
     var successDataToReturn: Any?
     
-    // Hatalı bir sonuç döndürmek için bu değişkene bir NetworkError ata.
     var failureErrorToReturn: NetworkError?
 
-    // MARK: - Conformance to NetworkManagingProtocol
     
     func performTask<T: Decodable>(_ request: NetworkRequest, decodeTo type: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) {
         
-        // 1. Hata senaryosunu kontrol et
         if let error = failureErrorToReturn {
             completion(.failure(error))
             return
         }
         
-        // 2. Başarı senaryosunu kontrol et
         if let data = successDataToReturn {
-            // Gelen 'Any' tipindeki veriyi, 'ViewModel'in beklediği 'T' tipine cast et.
             if let castedData = data as? T {
                 completion(.success(castedData))
             } else {
-                // Eğer testte yanlış tipte veri set ettiysek, bunu anlamak için çökert.
                 fatalError("MockNetworkManager'daki successDataToReturn, beklenen \(T.self) tipiyle uyumsuz.")
             }
             return
         }
         
-        // 3. Testi yazan kişi hiçbir şey set etmediyse, bunu anlamak için çökert.
         fatalError("MockNetworkManager için ne successDataToReturn ne de failureErrorToReturn ayarlanmadı.")
     }
     
-    // downloadImage için de aynı basit yapıyı kuralım.
     var mockImageResult: Result<Data, NetworkError>?
     
     func downloadImage(from url: URL, completion: @escaping (Result<Data, NetworkError>) -> Void) {
